3453a53af0ef1eac93be1d81b3e947cf28fc6d1c
Sinan Yuksel <sinanyuksel04@gmail.com>
2025-03-22 13:32:44 +0300

    SQL connection complete.

--- RestoranOtomasyonSistemi/RestoranOtomasyonSistemi/Form1.Designer.cs
+++ RestoranOtomasyonSistemi/RestoranOtomasyonSistemi/Form1.Designer.cs
@@ -28,35 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-
-
-
             button1 = new Button();
+            textBox1 = new TextBox();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(447, 227);
+            button1.Location = new Point(599, 352);
             button1.Name = "button1";
             button1.Size = new Size(75, 23);
             button1.TabIndex = 0;
             button1.Text = "button1";
             button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(359, 113);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(147, 23);
+            textBox1.TabIndex = 1;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(textBox1);
             Controls.Add(button1);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button button1;
+        private TextBox textBox1;
     }
 }
--- RestoranOtomasyonSistemi/RestoranOtomasyonSistemi/Form1.cs
+++ RestoranOtomasyonSistemi/RestoranOtomasyonSistemi/Form1.cs
@@ -1,5 +1,8 @@
+using Microsoft.Data.SqlClient;
+
 namespace RestoranOtomasyonSistemi
 {
+
     public partial class Form1 : Form
     {
         public Form1()
@@ -10,6 +13,44 @@ namespace RestoranOtomasyonSistemi
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string connectionString = "Server=localhost\\SQLExpress; Database=TestDB; Integrated Security=True; Encrypt=False;";
+
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    // Baðlantýyý aç
+                    connection.Open();
+
+                    string query = "SELECT TOP 100 YemekID, YemekAdi, Fiyat, Stok FROM Yemekler";
+                    // SqlCommand nesnesi ile sorguyu çalýþtýr
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            // Verileri okuyup ilk öðe için fiyatý TextBox'a yerleþtiriyoruz
+                            if (reader.Read())
+                            {
+                                // Ýlk öðenin fiyatýný alýyoruz
+                                decimal fiyat = reader.GetDecimal(2);  // Fiyat sütununu alýyoruz
+                                textBox1.Text = fiyat.ToString();
+                                // TextBox'a fiyatý yazýyoruz
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Hata durumunda mesajý göster
+                    Console.WriteLine("Hata: " + ex.Message);
+                }
+            }
         }
     }
 }
