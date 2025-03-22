1dfbfcc7265f50efab805d80aaf8157d8edaa4df
Sinan Yuksel <sinanyuksel04@gmail.com>
2025-03-22 18:23:42 +0300

    Fix partial class issue

--- RestoranOtomasyonSistemi/RestoranOtomasyonSistemi/BasketInfo.cs
+++ RestoranOtomasyonSistemi/RestoranOtomasyonSistemi/BasketInfo.cs
@@ -2,36 +2,31 @@
 
 namespace RestoranOtomasyonSistemi
 {
-
-    public partial class FoodOrderModule
+    public class BasketInfo
     {
-        public class BasketInfo
-        {
-            public List<FoodInfo> ReadyToOrderFoods = new List<FoodInfo>();
+        public List<FoodInfo> ReadyToOrderFoods = new List<FoodInfo>();
 
-            public void AddToBasket(int foodID)
-            { 
-                var dataBaseService = ServiceLocator.GetService<DataBaseService>();
-                ReadyToOrderFoods.Add(dataBaseService.GetFoodInfoById(foodID));
+        public void AddToBasket(int foodID)
+        { 
+            var dataBaseService = ServiceLocator.GetService<DataBaseService>();
+            ReadyToOrderFoods.Add(dataBaseService.GetFoodInfoById(foodID));
               
-            }
-
-            public void ClearBasket()
-            {
-                ReadyToOrderFoods?.Clear();
-            }
-
-            public decimal GetTotalAmount()
-            {
-                var totalAmount = 0m;
-                foreach (var item in ReadyToOrderFoods)
-                {
-                    totalAmount += item.FoodPrice;
-                }
-
-                return totalAmount;
-            }
         }
 
+        public void ClearBasket()
+        {
+            ReadyToOrderFoods?.Clear();
         }
+
+        public decimal GetTotalAmount()
+        {
+            var totalAmount = 0m;
+            foreach (var item in ReadyToOrderFoods)
+            {
+                totalAmount += item.FoodPrice;
+            }
+
+            return totalAmount;
+        }
+    }
     }
